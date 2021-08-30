4987d2315a96481021f0c96f23859cb88552d3cb
cjsrhd94 <cjsrhd94@gmail.com>
2021-08-31 01:21:01 +0900

    feat(SavingAccount.class): SavingAccount 클래스 생성자 및 메서드 구현

--- BankingSystem/src/account/SavingAccount.java
+++ BankingSystem/src/account/SavingAccount.java
@@ -5,22 +5,36 @@ import bank.Bank;
 import java.math.BigDecimal;
 
 //TODO: SavingAccount는 Account에서 상속을 받습니다.
-public class SavingAccount extends Account{
+public class SavingAccount extends Account {
     //TODO: 적금 계좌 클래스의 속성은 목표금액 속성을 추가로 가집니다.
     private BigDecimal goalAmount = new BigDecimal(100000);
 
-    public SavingAccount(){
+    public SavingAccount() {
         //TODO: 카테고리를 S로 설정해 줍니다.
+        this.category = "S";
     }
 
     public SavingAccount(String accNo, String owner, BigDecimal balance, BigDecimal goalAmount) {
         // TODO
+        super(accNo, owner, balance);
+        this.goalAmount = goalAmount;
+        this.category = "S";
+        this.isActive = true;
     }
 
     //TODO: GoalAmount getter 구현
+    public BigDecimal getGoalAmount() {
+        return goalAmount;
+    }
+
+    public void setGoalAmount(BigDecimal goalAmount) {
+        this.goalAmount = goalAmount;
+    }
 
     //TODO: getAccountInfo를 재정의하여 "목표 금액"도 노출해줍니다.
+    @Override
     public void getAccountInfo(Account account) {
         //TODO
+        System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원 | 목표 금액 %s원", category, accNo, owner, balance, goalAmount);
     }
 }
