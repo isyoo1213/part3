d99f266fb2b778b0f2d0d022c0b4804348cd499b
yangdongjue5510 <djwhy5510@gmail.com>
2021-08-30 17:03:00 +0900

    feat: SavingAccount class done.
    DONE:
    1. Constructor done.
    2. field appended and getter done.
    3. getAccountInfo override done.
    
    TODO:
    1. super class private field & sub class

--- BankingSystem/src/account/SavingAccount.java
+++ BankingSystem/src/account/SavingAccount.java
@@ -11,16 +11,26 @@ public class SavingAccount extends Account{
 
     public SavingAccount(){
         //TODO: 카테고리를 S로 설정해 줍니다.
+        this.setCategory("S");
     }
 
     public SavingAccount(String accNo, String owner, BigDecimal balance, BigDecimal goalAmount) {
         // TODO
+        this.setAccNo(accNo);
+        this.setOwner(owner);
+        this.setBalance(balance);
+        this.goalAmount = goalAmount;
     }
 
     //TODO: GoalAmount getter 구현
+    public BigDecimal getGoalAmount() {
+        return goalAmount;
+    }
 
     //TODO: getAccountInfo를 재정의하여 "목표 금액"도 노출해줍니다.
+    @Override
     public void getAccountInfo(Account account) {
         //TODO
+        System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원 | 목표 금액: %s원\n", getCategory(), getAccNo(), getOwner(), getGoalAmount(), goalAmount);
     }
 }
