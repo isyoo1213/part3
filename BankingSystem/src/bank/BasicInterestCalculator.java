06fa467db671ab26c20405c7bcb251b09a2b35f8
yangdongjue5510 <djwhy5510@gmail.com>
2021-08-31 13:48:07 +0900

    feat: InterestCalculator interface, BasicInterestCalculator class done.
    TODO:
    1. Do SavingInterestCalculator class
    2. Do CentralBank class

--- BankingSystem/src/bank/BasicInterestCalculator.java
+++ BankingSystem/src/bank/BasicInterestCalculator.java
@@ -9,6 +9,14 @@ public class BasicInterestCalculator{
         // TODO:
         //  예금 계좌의 경우 잔액이 1000만원 이상은 이자율이 50%,
         //  500만원 이상은 7%, 100만원 이상은 4%, 1만원 이상은 2%, 그 외에는 1% 입니다.
-        return null;
+        if (balance.compareTo(new BigDecimal("10000000")) >=0)
+            interest = new BigDecimal("0.5");
+        else if (balance.compareTo(new BigDecimal("5000000")) >=0)
+            interest = new BigDecimal(("0.07"));
+        else if (balance.compareTo(new BigDecimal("1000000")) >= 0)
+            interest = new BigDecimal("0.04");
+        else
+            interest = new BigDecimal("0.01");
+        return interest;
     }
 }
