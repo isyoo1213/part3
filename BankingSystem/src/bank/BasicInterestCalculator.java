bc33b2a589fbe662488c89e727da5d974ff1757d
bigdream96 <bigdream9609@gmail.com>
2021-08-31 15:30:40 +0900

    feat: InterestCalculator interface, class done.
    1. interest calculation interface
    2. Interest calculation implementation class

--- BankingSystem/src/bank/BasicInterestCalculator.java
+++ BankingSystem/src/bank/BasicInterestCalculator.java
@@ -2,13 +2,24 @@ package bank;
 
 import java.math.BigDecimal;
 
-// TODO: InterestCalculator 인터페이스 구현한 BasicInterestCalculator 클래스
-public class BasicInterestCalculator{
+public class BasicInterestCalculator implements InterestCalculator {
+    @Override
     public BigDecimal getInterest(BigDecimal balance) {
         BigDecimal interest;
-        // TODO:
-        //  예금 계좌의 경우 잔액이 1000만원 이상은 이자율이 50%,
-        //  500만원 이상은 7%, 100만원 이상은 4%, 1만원 이상은 2%, 그 외에는 1% 입니다.
-        return null;
+
+        double value = balance.doubleValue();
+        if(value < 10_000) {
+            interest = balance.multiply(new BigDecimal("0.01"));
+        } else if(value < 1_000_000) {
+            interest = balance.multiply(new BigDecimal("0.02"));
+        } else if(value < 5_000_000) {
+            interest = balance.multiply(new BigDecimal("0.04"));
+        } else if(value < 10_000_000) {
+            interest = balance.multiply(new BigDecimal("0.07"));
+        } else {
+            interest = balance.multiply(new BigDecimal("0.5"));
+        }
+
+        return interest;
     }
 }
