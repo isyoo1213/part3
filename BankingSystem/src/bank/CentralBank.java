88d5a32020060d7fbea76c4864745f3aa80a00fe
yangdongjue5510 <djwhy5510@gmail.com>
2021-08-31 15:31:03 +0900

    feat: CentralBank class done.
    TODO:
    1. Bank class
    2. SavingBank class

--- BankingSystem/src/bank/CentralBank.java
+++ BankingSystem/src/bank/CentralBank.java
@@ -2,16 +2,35 @@ package bank;
 
 import account.Account;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class CentralBank {
     //은행시스템의 계좌들을 관리하는 중앙은행 클래스입니다.
     //TODO: 싱글톤 패턴으로 설계합니다.
+    private static CentralBank centralBank = null;
+    private CentralBank(){}
+
     //TODO: accountList(Account로 이루어진 ArrayList)
+    private ArrayList<Account> accountList = new ArrayList<>();
+
     //TODO: BANK_NAME(은행명)
+    public final String BANK_NAME = "CentralBank";
 
     //TODO: getInstance 함수
+    public static CentralBank getInstance(){
+        if (centralBank == null){
+            centralBank = new CentralBank();
+        }
+        return centralBank;
+    }
 
     //TODO: accountList getter/setter
+    public ArrayList<Account> getAccountList() {
+        return accountList;
+    }
 
+    public void setAccountList(ArrayList<Account> accountList) {
+        this.accountList = accountList;
+    }
 }
