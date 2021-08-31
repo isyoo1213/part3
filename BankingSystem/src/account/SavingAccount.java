db1f2a8b6bada0fe172e4eb7637dd16bd3b9edf7
bigdream96 <bigdream9609@gmail.com>
2021-08-31 15:12:55 +0900

    feat: Account.java SavingAccount.java done.
    1. member variable
    2. member getter/setter
    3. getAccountInfo method
    4. deposit, withdraw method

--- BankingSystem/src/account/SavingAccount.java
+++ BankingSystem/src/account/SavingAccount.java
@@ -1,26 +1,30 @@
 package account;
 
-import bank.Bank;
-
 import java.math.BigDecimal;
 
-//TODO: SavingAccount는 Account에서 상속을 받습니다.
-public class SavingAccount extends Account{
-    //TODO: 적금 계좌 클래스의 속성은 목표금액 속성을 추가로 가집니다.
+public class SavingAccount extends Account {
     private BigDecimal goalAmount = new BigDecimal(100000);
 
     public SavingAccount(){
-        //TODO: 카테고리를 S로 설정해 줍니다.
+        super.category = "S";
     }
 
     public SavingAccount(String accNo, String owner, BigDecimal balance, BigDecimal goalAmount) {
-        // TODO
+        this.accNo = accNo;
+        this.owner = owner;
+        this.balance = balance;
+        this.goalAmount = goalAmount;
     }
 
-    //TODO: GoalAmount getter 구현
+    public BigDecimal getGoalAmount() {
+        return goalAmount;
+    }
 
-    //TODO: getAccountInfo를 재정의하여 "목표 금액"도 노출해줍니다.
+    public void setGoalAmount(BigDecimal goalAmount) {
+        this.goalAmount = goalAmount;
+    }
+
     public void getAccountInfo(Account account) {
-        //TODO
+        System.out.printf("\n%s | %s | %s | %s원 | %s원", category, accNo, owner, balance, goalAmount);
     }
 }
