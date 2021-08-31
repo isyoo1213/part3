25cc0bbdae622c71ca5a66e01955fa193e216bae
cjsrhd94 <cjsrhd94@gmail.com>
2021-08-31 21:41:44 +0900

    feat(CentralBank.class): 싱글톤 패턴 구현

--- BankingSystem/src/bank/CentralBank.java
+++ BankingSystem/src/bank/CentralBank.java
@@ -2,6 +2,7 @@ package bank;
 
 import account.Account;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 public class CentralBank {
@@ -9,9 +10,27 @@ public class CentralBank {
     //TODO: 싱글톤 패턴으로 설계합니다.
     //TODO: accountList(Account로 이루어진 ArrayList)
     //TODO: BANK_NAME(은행명)
+    private static ArrayList<Account> accountList = new ArrayList<>();
+    private static CentralBank instance = new CentralBank();
+
+    private CentralBank() {
+
+    }
 
     //TODO: getInstance 함수
+    public static CentralBank getInstance(){
+        if(instance == null){
+            instance = new CentralBank();
+        }
+        return instance;
+    }
 
     //TODO: accountList getter/setter
+    public ArrayList<Account> getAccountList() {
+        return accountList;
+    }
 
+    public void setAccountList(ArrayList<Account> accountList) {
+        this.accountList = accountList;
+    }
 }
