4bda79ddfadde9dd0248e073847e4b45f08590e5
limhaekyu <limk9500@gmail.com>
2021-09-01 14:38:05 +0900

    feat: 계좌번호 검색 후 적금계좌이면 적금계좌 출금 메소드 호출기능구현, deposit 메서드 구현

--- BankingSystem/src/bank/Bank.java
+++ BankingSystem/src/bank/Bank.java
@@ -1,11 +1,9 @@
 package bank;
 
 import account.Account;
-import account.SavingAccount;
 
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
-import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -14,6 +12,9 @@ public class Bank {
     protected static Scanner scanner = new Scanner(System.in);
     protected static int seq = 0;
     public static DecimalFormat df = new DecimalFormat("#,###");
+    private Account depositAccount;
+    private BigDecimal balance;
+
 
     // 뱅킹 시스템의 기능들
     public void withdraw() throws Exception {
@@ -21,12 +22,19 @@ public class Bank {
         //TODO: key, value 형태의 HashMap을 이용하여 interestCalculators 구현
         //여기서 key: category, value: 각 category의 InterestCalculator 인스턴스
 
+
+
         // 계좌번호 입력
         Account account;
         while(true){
             System.out.println("\n출금하시려는 계좌번호를 입력하세요.");
             String accNo = scanner.next();
             // TODO: 검색 -> 적금 계좌이면 적금 계좌의 출금 메소드 호출 -> 완료시 break
+            if(findAccount(accNo).getCategory() == "S"){
+                SavingBank.withdraw();
+                break;
+            }
+
 
         }
         // 출금처리
@@ -42,10 +50,22 @@ public class Bank {
     public void deposit(){
         //TODO: 입금 메서드 구현
         // 존재하지 않는 계좌이면 다시 물어보기
+
         System.out.println("\n입금하시려는 계좌번호를 입력해주세요.");
+        String accNo = scanner.next();
+        depositAccount = findAccount(accNo);
+        if(depositAccount == null){
+            deposit();
+        }
 
         // TODO: 입금 처리
         System.out.println("\n입금할 금액을 입력하세요.");
+        BigDecimal depositMoney = scanner.nextBigDecimal();
+        balance = depositAccount.getBalance();
+        balance.add(depositMoney);
+
+
+
 
     }
 
