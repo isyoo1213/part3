21c611319b3b6bbdc959685867aa7498c7be2bad
jspgmp <mmriann@gmail.com>
2021-09-01 20:17:16 +0900

    Feat: CentralBank class update
    add
    Singleton Pattern
    ArrayList<>
    bank name(not used)
    getAccountList
    setAccountList

--- BankingSystem/src/bank/CentralBank.java
+++ BankingSystem/src/bank/CentralBank.java
@@ -1,17 +1,36 @@
 package bank;
 
 import account.Account;
-
 import java.util.ArrayList;
 
 public class CentralBank {
     //은행시스템의 계좌들을 관리하는 중앙은행 클래스입니다.
     //TODO: 싱글톤 패턴으로 설계합니다.
-    //TODO: accountList(Account로 이루어진 ArrayList)
-    //TODO: BANK_NAME(은행명)
-
     //TODO: getInstance 함수
+    private static CentralBank instance = new CentralBank();
+    private CentralBank() {}
+
+    public static CentralBank getInstance() {
+        if(instance == null) {
+            instance = new CentralBank();
+        }
+        return instance;
+    }
+
+    //TODO: accountList(Account로 이루어진 ArrayList)
+    ArrayList<Account> accountList = new ArrayList<>();
+
+
+    //TODO: BANK_NAME(은행명)
+    public static String BANK_NAME = "중앙은행";
+
 
     //TODO: accountList getter/setter
+    public ArrayList<Account> getAccountList() {
+        return accountList;
+    }
 
+    public void setAccountList(ArrayList<Account> accountList) {
+        this.accountList = accountList;
+    }
 }
