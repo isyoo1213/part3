a40074a921c2b476ada7fe0131e733625b08a1ff
cjsrhd94 <cjsrhd94@gmail.com>
2021-09-01 16:48:01 +0900

    feat: SavingInterestCalculator 클래스 getInterest 메서드 구현

--- BankingSystem/src/bank/SavingInterestCalculator.java
+++ BankingSystem/src/bank/SavingInterestCalculator.java
@@ -3,10 +3,15 @@ package bank;
 import java.math.BigDecimal;
 
 // TODO: InterestCalculator 인터페이스 구현한 SavingInterestCalculator 클래스
-public class SavingInterestCalculator{
+public class SavingInterestCalculator implements InterestCalculator{
     public BigDecimal getInterest(BigDecimal balance) {
         BigDecimal interest;
         // TODO: 적금 계좌의 경우 잔액이 100만원 이상은 이자율이 50%, 그 외에는 1% 입니다.
+        if (balance.compareTo(BigDecimal.valueOf(1000000)) >= 0) {
+            interest = BigDecimal.valueOf(0.5);
+        } else {
+            interest = BigDecimal.valueOf(0.01);
+        }
         return null;
     }
 }
