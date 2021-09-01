10052e663f9b3fc77e87e93e58f0f9a7ad13c879
cfhj8520 <cfhj8520@naver.com>
2021-09-01 20:07:22 +0900

    SavingAccount.java 멤버변수 선언

--- BankingSystem/src/account/SavingAccount.java
+++ BankingSystem/src/account/SavingAccount.java
@@ -8,6 +8,11 @@ import java.math.BigDecimal;
 public class SavingAccount extends Account{
     //TODO: 적금 계좌 클래스의 속성은 목표금액 속성을 추가로 가집니다.
     private BigDecimal goalAmount = new BigDecimal(100000);
+    private String category;//category;             // String 계좌 종류
+    private String accNo;// accNo;               // String 계좌번호
+    private String owner;// owner;               // String 소유자
+    private BigDecimal balance;// balance;             // BigDecimal 잔액
+    private boolean isActive;// isActive;            // boolean 활성화 여부
 
     public SavingAccount(){
         //TODO: 카테고리를 S로 설정해 줍니다.
