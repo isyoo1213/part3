66d94ea90d845de71f3d09e89c25b0bd8b5e05dc
cfhj8520 <cfhj8520@naver.com>
2021-09-02 14:39:27 +0900

    withdraw() 적금 계좌 잔액 확인 후 예외처리 구현

--- BankingSystem/src/bank/SavingBank.java
+++ BankingSystem/src/bank/SavingBank.java
@@ -11,6 +11,15 @@ public class SavingBank extends Bank {
     public void withdraw(SavingAccount account) throws Exception{
         // TODO: Account의 출금 메서드에서 잔액/목표 금액 체크하여 조금 다르게 구현
         // throws Exception 적금 계좌는 잔액이 목표 금액(%s원) 이상이어야 출금 가능합니다.
+        try{
+            if(account.getGoalAmount().compareTo(account.getBalance()) < 0){
+                throw new Exception();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+
+            System.out.printf("적금 계좌는 잔액이 목표 금액(%s원) 이상이어야 출금 가능합니다.", account.getGoalAmount());
+        }
     }
     // TODO: 목표금액을 입력받아서 SavingAccount 객체 생성하도록 재정의
     @Override
