1f3feacd0869b8a5064c5815d33b2bab051d2673
kho903 <gmldnr2222@naver.com>
2021-09-04 08:42:58 +0900

    fix: 10000000 -> 1000000

--- BankingSystem/src/bank/SavingInterestCalculator.java
+++ BankingSystem/src/bank/SavingInterestCalculator.java
@@ -7,7 +7,7 @@ public class SavingInterestCalculator implements InterestCalculator{
     public BigDecimal getInterest(BigDecimal balance) {
         BigDecimal interest;
         // TODO: 적금 계좌의 경우 잔액이 100만원 이상은 이자율이 50%, 그 외에는 1% 입니다.
-        if (balance.compareTo(new BigDecimal("10000000")) >= 0)
+        if (balance.compareTo(new BigDecimal("1000000")) >= 0)
             interest = new BigDecimal("0.5");
         else
             interest = new BigDecimal("0.01");
