c38c09d1b66538b61b35af83e34482c219fe3a62
kho903 <gmldnr2222@naver.com>
2021-09-03 20:57:44 +0900

    feat: setGoalAmount in SavingAccount.java

--- BankingSystem/src/account/SavingAccount.java
+++ BankingSystem/src/account/SavingAccount.java
@@ -5,11 +5,11 @@ import bank.Bank;
 import java.math.BigDecimal;
 
 //TODO: SavingAccount는 Account에서 상속을 받습니다.
-public class SavingAccount extends Account{
+public class SavingAccount extends Account {
     //TODO: 적금 계좌 클래스의 속성은 목표금액 속성을 추가로 가집니다.
     private BigDecimal goalAmount = new BigDecimal(100000);
 
-    public SavingAccount(){
+    public SavingAccount() {
         //TODO: 카테고리를 S로 설정해 줍니다.
         this.setCategory("S");
     }
@@ -26,9 +26,13 @@ public class SavingAccount extends Account{
         return goalAmount;
     }
 
+    public void setGoalAmount(BigDecimal goalAmount) {
+        this.goalAmount = goalAmount;
+    }
+
     //TODO: getAccountInfo를 재정의하여 "목표 금액"도 노출해줍니다.
     public void getAccountInfo(Account account) {
         //TODO
-        System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원 | 목표 금액 : %s", this.getCategory(), this.getAccNo(), this.getOwner(), this.getBalance(), this.getGoalAmount());
+        System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원 | 목표 금액 : %s\n", this.getCategory(), this.getAccNo(), this.getOwner(), this.getBalance(), this.getGoalAmount());
     }
 }
