78d18b07487224a94909e1cbd1000a57f99ae5ed
isyoo1213 <moethns@gmail.com>
2021-09-05 20:58:29 +0900

    Feat: create createAccount method on SavingBank Class
    1. SavingBank의 createAccount 메서드를 작성했습니다.
    
    Exception을 완전히 숙지하지 못해서 계좌주 이름을 입력 받을 시, 완성형
    한글이 아닐 경우 직접 만든 OwnerException을 처리하도록 구성했습니다.
    
    2. 적금 목표액을 입력받고 설정하도록 구성했습니다.
    
    100만원 이상의 적금 목표액을 사용자가 입력할 수 있도록 구성했습니다.

--- BankingSystem/src/bank/SavingBank.java
+++ BankingSystem/src/bank/SavingBank.java
@@ -12,13 +12,43 @@ public class SavingBank extends Bank {
         // TODO: Account의 출금 메서드에서 잔액/목표 금액 체크하여 조금 다르게 구현
         // throws Exception 적금 계좌는 잔액이 목표 금액(%s원) 이상이어야 출금 가능합니다.
     }
+
     // TODO: 목표금액을 입력받아서 SavingAccount 객체 생성하도록 재정의
+
     @Override
-    public SavingAccount createAccount() throws NoSuchElementException{
-        try{
-            return account;
-        }catch (){
-            //TODO: 오류 throw
+    public SavingAccount createAccount() throws OwnerException{
+
+        System.out.println("\n적금 계좌 생성을 시작합니다.");
+        System.out.println("생성하시려는 적금계좌의 계좌주 성함을 입력해주세요.");
+        String newOwner = scanner.next();
+
+        BigDecimal newGoalAmount = new BigDecimal("0");
+        boolean createActive = true;
+        while(createActive){
+            System.out.println("\n적금 계좌의 목표 금액을 입력해주세요.(100만원 이상)");
+            String strGoalAmount = scanner.next();
+            if (Integer.parseInt(strGoalAmount) < 1000000){
+                System.out.println("100만원 이상의 금액을 입력해주세요.");
+                createActive = false;
+            } else {
+                newGoalAmount = new BigDecimal(strGoalAmount);
+                createActive = false;
+            }
         }
+
+        String newAccNo = String.format("%04d", this.getCountAcc());
+        BigDecimal newBalance = new BigDecimal("0");
+
+        SavingAccount account = new SavingAccount(newAccNo, newOwner, newBalance, newGoalAmount);
+        account.setActive(true);
+        account.setCategory("S");
+
+        System.out.printf("\n%s님 적금 계좌가 발급되었습니다.\n", newOwner);
+        account.getAccountInfo(account); //출력
+
+        this.setCountAcc(this.getCountAcc() + 1);
+
+        return account;
+
     }
 }
