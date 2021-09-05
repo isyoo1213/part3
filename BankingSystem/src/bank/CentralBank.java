6e7a57f52d428a087222f8fd425aa81be0acad72
lowgiant <lowgiant@gmail.com>
2021-09-05 17:27:19 +0900

    feat: Central bank class

--- BankingSystem/src/bank/CentralBank.java
+++ BankingSystem/src/bank/CentralBank.java
@@ -7,11 +7,28 @@ import java.util.ArrayList;
 public class CentralBank {
     //은행시스템의 계좌들을 관리하는 중앙은행 클래스입니다.
     //TODO: 싱글톤 패턴으로 설계합니다.
+    private static CentralBank instance = new CentralBank();
     //TODO: accountList(Account로 이루어진 ArrayList)
+    ArrayList<Account> accountList;
+
+    private CentralBank() {
+    }
+
     //TODO: BANK_NAME(은행명)
+    private static String BANK_NAME = "중앙은행";
 
     //TODO: getInstance 함수
+    public static CentralBank getInstance() {
+        return instance;
+    }
 
     //TODO: accountList getter/setter
+    public ArrayList<Account> getAccountList() {
+        return accountList;
+    }
+
+    public void setAccountList(ArrayList<Account> accountList) {
+        this.accountList = accountList;
+    }
 
 }
