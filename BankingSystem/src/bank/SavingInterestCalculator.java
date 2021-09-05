47911c771e1815b0eacb21dc13430c6b35b065f0
isyoo1213 <moethns@gmail.com>
2021-09-05 21:19:52 +0900

    Feat: create features on SavingInterestCalculator Class
    1. InterestCalculator를 구현한 SavingInterestCalculator 클래스를
       작성했습니다.
    
    적금 계좌의 이자비율과 기준 잔액에 따라 이자금액을 계산하고 interest
    변수에 할당하도록 구성했습니다.

--- BankingSystem/src/bank/SavingInterestCalculator.java
+++ BankingSystem/src/bank/SavingInterestCalculator.java
@@ -3,10 +3,23 @@ package bank;
 import java.math.BigDecimal;
 
 // TODO: InterestCalculator 인터페이스 구현한 SavingInterestCalculator 클래스
-public class SavingInterestCalculator{
-    public BigDecimal getInterest(BigDecimal balance) {
+
+public class SavingInterestCalculator implements InterestCalculator{
+
+    // TODO: 적금 계좌의 경우 잔액이 100만원 이상은 이자율이 50%, 그 외에는 1% 입니다.
+
+    @Override
+    public BigDecimal getInsterest(BigDecimal balance) {
+
         BigDecimal interest;
-        // TODO: 적금 계좌의 경우 잔액이 100만원 이상은 이자율이 50%, 그 외에는 1% 입니다.
-        return null;
+
+        if(balance.compareTo(tier3) >= 0){
+            interest = balance.multiply(tier1Ratio);
+        } else {
+            interest = balance.multiply(tier5Ratio);
+        }
+        return interest;
+
     }
+
 }
