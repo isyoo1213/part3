38e0d638f8957f482e63aba9e89eb8f104b47f33
isyoo1213 <moethns@gmail.com>
2021-09-05 20:24:54 +0900

    Feat: create features on Account Class
    1. Account 클래스의 멤버변수들을 선언했습니다.
    2. 입력값을 받지 않는 생성자와 계좌정보를 입력받아오는 생성자를
       추가했습니다.
    3. 각 멤버변수들의 getter/setter를 추가했습니다.
    4. getAccountInfo 메서드의 출력내용을 추가했습니다.
    5. withdraw 메서드의 내용을 추가했습니다.
    6. deposit 메서드의 내용을 추가했습니다.

--- BankingSystem/src/account/Account.java
+++ BankingSystem/src/account/Account.java
@@ -12,27 +12,79 @@ public class Account {
     // balance;             // BigDecimal 잔액
     // isActive;            // boolean 활성화 여부
 
+    protected String category;
+    protected String accNo;
+    protected String owner;
+    protected BigDecimal balance;
+    protected boolean isActive;
+
     public Account() {
         //TODO: 일반 계좌의 활성화 여부를 True로, 계좌 종류를 "N"(NORMAL을 의미) 설정해줍니다.
+        this.isActive = true;
+        this.category = "N";
     }
     public Account(String accNo, String owner, BigDecimal balance) {
-        //TODO
+        this.accNo = accNo;
+        this.owner = owner;
+        this.balance = balance;
     }
 
     // TODO: 일반 계좌 클래스의 각 속성에 getter/setter를 제공합니다.
 
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAccNo() {
+        return accNo;
+    }
+
+    public void setAccNo(String accNo) {
+        this.accNo = accNo;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
     public void getAccountInfo(Account account){
         //TODO: 계좌의 기본 정보를 아래 형태로 출력해줍니다.
         //계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원
+        System.out.printf("\n계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원\n", account.category, account.accNo, account.owner, account.balance);
     }
 
     public BigDecimal withdraw(BigDecimal amount) throws Exception{
         //TODO: 출금액을 받아서 출금하는 기본 메소드입니다. this를 이용해 구현해보세요.
-
+        this.setBalance(this.getBalance().subtract(amount));
         return amount;
     }
     public BigDecimal deposit(BigDecimal amount){
         //TODO: 입금액을 받아서 입금하는 기본 메소드입니다. this를 이용해 구현해보세요.
+        this.setBalance(this.getBalance().add(amount));
         return amount;
     }
 }
