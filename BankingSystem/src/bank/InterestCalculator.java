f7de37960468c455b9ebafa49d154f165623c0f2
isyoo1213 <moethns@gmail.com>
2021-09-05 21:13:13 +0900

    Feat: create features on InterestCalculator Interface
    1. InterestCalculator 인터페이스를 구성했습니다.
    
    계좌 종류 별 이자 비율 적용 기준 금액과 이자비율을 tier1~4와
    tier1~5Ratio 변수에 담았습니다.
    
    계좌 종류 별 이자계산 클래스에서 구현할getInterest 추상 메서드를 선언했습니다.

--- BankingSystem/src/bank/InterestCalculator.java
+++ BankingSystem/src/bank/InterestCalculator.java
@@ -4,4 +4,17 @@ import java.math.BigDecimal;
 
 public interface InterestCalculator {
     //TODO: 잔액에 대한 이자 금액을 반환하는 메서드 ( BigDecimal getInterest(balance))를 선언합니다.
+
+    BigDecimal tier1 = new BigDecimal("10000000");
+    BigDecimal tier2 = new BigDecimal("5000000");
+    BigDecimal tier3 = new BigDecimal("1000000");
+    BigDecimal tier4 = new BigDecimal("10000");
+    BigDecimal tier1Ratio = new BigDecimal("0.5");
+    BigDecimal tier2Ratio = new BigDecimal("0.07");
+    BigDecimal tier3Ratio = new BigDecimal("0.04");
+    BigDecimal tier4Ratio = new BigDecimal("0.02");
+    BigDecimal tier5Ratio = new BigDecimal("0.01");
+
+    BigDecimal getInsterest(BigDecimal balance);
+
 }
