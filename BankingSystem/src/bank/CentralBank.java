38cb2b5903eacf3676f0cc2c3a902ee7c10e009c
limhaekyu <limk9500@gmail.com>
2021-09-05 18:43:04 +0900

    feat: CentralBank 클래스 구현

--- BankingSystem/src/bank/CentralBank.java
+++ BankingSystem/src/bank/CentralBank.java
@@ -9,9 +9,27 @@ public class CentralBank {
     //TODO: 싱글톤 패턴으로 설계합니다.
     //TODO: accountList(Account로 이루어진 ArrayList)
     //TODO: BANK_NAME(은행명)
+    private static CentralBank centralBank = null;
+
+    private CentralBank(){};
+
+    ArrayList<Account> accountList = new ArrayList<>();
+    public static String BANK_NAME;
 
     //TODO: getInstance 함수
+    public static CentralBank getInstance(){
+        if(centralBank == null){
+            centralBank = new CentralBank();
+        }
+        return centralBank;
+    }
 
     //TODO: accountList getter/setter
+    public void setAccountList(ArrayList accountList){
+        this.accountList = accountList;
+    }
+    public ArrayList<Account> getAccountList(){
+        return accountList;
+    }
 
 }
