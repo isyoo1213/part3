7d7f36e6eb4961092dc9ca81e009f65b1f6d6b67
isyoo1213 <moethns@gmail.com>
2021-09-08 23:16:25 +0900

    fix: Change withdraw method ans createAccount method on SavingBank Class
    1. 적금계좌의 출금 흐름에 Bank 클래스의  withdraw 메서드를 재사용하기 위해
    SavingBank 클래스의 withdraw 메서드를 적금 목표 금액을 확인하는 기능으로
    축소함
    
    2. 적금계좌 생성시 목표금액의 기준을 만족하지 못하더라도 계좌가 생성되는
    오류를 수정하기 위해 SavingBank의 createAccount 메서드를 수정함
    
    3. 적금계좌 생성시 계좌주 성명과 목표금액을 입력받을 때 예외처리를 추가함

--- BankingSystem/src/bank/SavingBank.java
+++ BankingSystem/src/bank/SavingBank.java
@@ -5,56 +5,94 @@ import java.math.BigDecimal;
 
 public class SavingBank extends Bank {
 
-    public void withdraw(SavingAccount account) throws AmountException {
+    public boolean withdraw(SavingAccount account) throws AmountException {
+
+        boolean savingWithdrawActive = true;
 
         // TODO: Account의 출금 메서드에서 잔액/목표 금액 체크하여 조금 다르게 구현
         // throws Exception 적금 계좌는 잔액이 목표 금액(%s원) 이상이어야 출금 가능합니다.
 
         try {
             if (account.getBalance().compareTo(account.getGoalAmount()) < 0) {
+                savingWithdrawActive = false;
                 throw new AmountException("적금 계좌의 잔액이 적금 목표 금액을 채우지 못했습니다.");
             }
         } catch (AmountException e){
             System.out.println(e.getMessage());
         }
+        return savingWithdrawActive;
     }
 
     // TODO: 목표금액을 입력받아서 SavingAccount 객체 생성하도록 재정의
 
     @Override
-    public SavingAccount createAccount() throws OwnerException{
+    public SavingAccount createAccount() throws OwnerException, AmountException {
+
+        boolean savingCreateAccountActive = true;
+        boolean createActive = true;
+        String newOwner = null;
+        BigDecimal newGoalAmount = new BigDecimal("0");
 
         System.out.println("\n적금 계좌 생성을 시작합니다.");
-        System.out.println("생성하시려는 적금계좌의 계좌주 성함을 입력해주세요.");
-        String newOwner = scanner.next();
 
-        BigDecimal newGoalAmount = new BigDecimal("0");
-        boolean createActive = true;
         while(createActive){
-            System.out.println("\n적금 계좌의 목표 금액을 입력해주세요.(100만원 이상)");
-            String strGoalAmount = scanner.next();
-            if (Integer.parseInt(strGoalAmount) < 1000000){
-                System.out.println("100만원 이상의 금액을 입력해주세요.");
-                createActive = false;
-            } else {
-                newGoalAmount = new BigDecimal(strGoalAmount);
-                createActive = false;
+
+            System.out.println("생성하시려는 적금계좌의 계좌주 성함을 입력해주세요.");
+            newOwner = scanner.next();
+
+            try {
+                if (!newOwner.matches("[가-힣].+")) {
+                    createActive = true;
+                    throw new OwnerException("이름은 완성된 한글로만 입력할 수 있습니다.");
+                } else {
+                    while(createActive){
+                        System.out.println("\n적금 계좌의 목표 금액을 입력해주세요.(100만원 이상)");
+                        String strGoalAmount = scanner.next();
+
+                        try {
+                            if (!strGoalAmount.matches("[0-9]+")) {
+                                createActive = true;
+                                throw new AmountException("금액은 0 이상의 소수점을 제외한 숫자 0~9의 조합으로만 입력해주세요.");
+                            } else if (Integer.parseInt(strGoalAmount) < 1000000) {
+                                createActive = true;
+                                throw new AmountException("100만원 이상의 금액을 입력해주세요.");
+                            } else {
+                                savingCreateAccountActive = true;
+                                newGoalAmount = new BigDecimal(strGoalAmount);
+                                createActive = false;
+                            }
+                        } catch (AmountException e){
+                            savingCreateAccountActive = false;
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                }
+            } catch (OwnerException e) {
+                savingCreateAccountActive = false;
+                System.out.println(e.getMessage());
             }
+
         }
 
-        String newAccNo = String.format("%04d", this.getSeq());
-        BigDecimal newBalance = new BigDecimal("0");
+        if(savingCreateAccountActive) {
+            //적금 계좌번호 생성
+            String newAccNo = String.format("%04d", this.getSeq());
+            BigDecimal newBalance = new BigDecimal("0");
+            //적금 계좌 인스턴스 생성
+            SavingAccount account = new SavingAccount(newAccNo, newOwner, newBalance, newGoalAmount);
+            account.setActive(true);
+            account.setCategory("S");
 
-        SavingAccount account = new SavingAccount(newAccNo, newOwner, newBalance, newGoalAmount);
-        account.setActive(true);
-        account.setCategory("S");
+            System.out.printf("\n%s님 적금 계좌가 발급되었습니다.\n", newOwner);
+            account.getAccountInfo(account); //출력
 
-        System.out.printf("\n%s님 적금 계좌가 발급되었습니다.\n", newOwner);
-        account.getAccountInfo(account); //출력
+            this.setSeq(this.getSeq() + 1);
 
-        this.setSeq(this.getSeq() + 1);
+            return account;
 
-        return account;
+        } else {
+            return null;
+        }
 
     }
 }
